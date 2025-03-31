1dfa37ec331dddf4144082149dc4462a8950c152
brobro332 <332bro@naver.com>
2025-03-31 23:25:57 +0900

    프로젝트 : 프로젝트 생성 및 의존성 주입

---
+++ src/main/java/kr/co/samsami_hub/SamsamiHubApplication.java
@@ -0,0 +1,13 @@
+package kr.co.samsami_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsamiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsamiHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/samsami_hub/SamsamiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.samsami_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsamiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
